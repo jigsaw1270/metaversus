74f1d037d5bff0a63d1e23d1f4eafa8223bcc826
jigsaw1270 <nomadshomoy@gmail.com>
2023-08-24 21:51:21 +0600

    about done

--- app/page.js
+++ app/page.js
@@ -5,13 +5,24 @@ const Page = () => (
   <div className='overflow-hidden bg-primary-black'>
     <Navbar />
     <Hero />
+    <div className="relative">
     <About />
+    <div className='z-0 gradient-03'/>
     <Explore />
+    </div>
+    <div className="relative">
     <GetStarted />
+    <div className='z-0 gradient-04'/>
     <WhatsNew />
+    </div>
+   
+ 
     <World />
+    <div className="relative">
     <Insights />
-    <Feedback />
+    <div className='z-0 gradient-04'/>
+    <Feedback /></div>
+   
     <Footer />
   </div>
 );
